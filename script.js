99d8b0b940a2104ff5bdfd92a19055eb0eeee466
tamanna096 <tamannamustari365@gmail.com>
2025-08-28 23:20:25 +0600

    services object add

---
+++ script.js
@@ -0,0 +1,12 @@
+// Services Data
+const services = [
+  { id:'fire', icon:'🚒', name:'National Emergency Number', eng:'Emergency Number', number:'999', category:'All' },
+  { id:'police', icon:'👮', name:'Police Helpline Number', eng:'Police', number:'999', category:'Police' },
+  { id:'ambulance', icon:'🚑', name:'Fire Service Number', eng:'Fire Service', number:'999', category:'Fire' },
+  { id:'rail', icon:'🚆', name:'Ambulance Service', eng:'Ambulance', number:'1994-999999', category:'Health' },
+  { id:'acc', icon:'⚖️', name:'Women & Child Helpline', eng:'Women & Child Helpline', number:'109', category:'Help' },
+  { id:'national', icon:'📞', name:'Anti-Corruption Helpline', eng:'Anti-Corruption', number:'106', category:'Govt.' },
+  { id:'electricity', icon:'🚆', name:'Electricity Helpline', eng:'Electricity Outage', number:'16216', category:'Electricity' },
+  { id:'ngo', icon:'⚖️', name:'Brac Helpline', eng:'Brac', number:'16445', category:'NGO' },
+  { id:'travel', icon:'📞', name:'Bangladesh Railway Helpline ', eng:'Bangladesh Railway', number:'163', category:'Travel' }
+];
